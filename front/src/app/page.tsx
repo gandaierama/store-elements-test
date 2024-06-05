d6e119dc481cab7ec7c909ca658adfc3dbc30e50
Daniel Batista Galvão <danielbatistagalvao@hotmail.com>
2024-06-05 03:21:39 -0300

    Button

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 
 // or less ideally
-import { Button, Stack } from 'react-bootstrap';
+import { Button, Stack, Card } from 'react-bootstrap';
 
 export default function Home() {
   return (
@@ -25,6 +25,18 @@ export default function Home() {
             Comprar
           </Button>
         </Stack>
+
+        <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
       </div>
 
       </div>
