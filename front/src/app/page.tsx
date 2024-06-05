c814a71453f3cd1ecc53652e7f732ab42755e4da
Daniel Batista Galvão <danielbatistagalvao@hotmail.com>
2024-06-05 02:41:49 -0300

    Button

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
+
+// or less ideally
+import { Button, Stack } from 'react-bootstrap';
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -9,6 +13,19 @@ export default function Home() {
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
        
 <div>Teste de elementos</div>
+
+
+      <div>
+        
+        <Stack direction="horizontal" gap={2}>
+          <Button as="a" variant="primary">
+            Detalhes
+          </Button>
+          <Button as="a" variant="success">
+            Comprar
+          </Button>
+        </Stack>;
+      </div>
 
       </div>
     </main>
