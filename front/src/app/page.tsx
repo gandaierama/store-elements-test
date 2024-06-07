5578f8e47d463f4a7b4ab7af6c42e870fb2d612d
Daniel Batista Galvão <danielbatistagalvao@hotmail.com>
2024-06-07 02:20:48 -0300

    base

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -2,20 +2,16 @@ import Image from "next/image";
 
 
 // or less ideally
-import { Button, Stack, Card } from 'react-bootstrap';
+import { Button, Stack, Container } from 'react-bootstrap';
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main >
       
 
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-       
-<div>Teste de elementos</div>
-
-
-      <div>
+      <Container>
         
         <Stack direction="horizontal" gap={2}>
           <Button as="a" variant="primary">
@@ -26,20 +22,9 @@ export default function Home() {
           </Button>
         </Stack>
 
-        <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-      </div>
+ 
 
-      </div>
+      </Container>
     </main>
-  );
+  ); 
 }
